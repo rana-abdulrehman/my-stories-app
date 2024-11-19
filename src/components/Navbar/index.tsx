50fd46bd688b50174f43478924a7e3084a8c107c
rana-abdulrehman <ranaabdulrehman4914@gmail.com>
2024-11-19 19:53:26 +0500

    Task: FrontEnd(Navbar page) zindex bug fixed

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
     };
 
     return (
-        <nav className= "sticky top-0 bg-slate-200 text-gray-900 body-font ">
+        <nav className= "sticky top-0 z-[100] bg-slate-200 text-gray-900 body-font ">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row justify-between items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
